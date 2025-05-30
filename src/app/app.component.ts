18a3af6e547644d24ae8c800eaef0323f09cb11d
Ricardo Alberto Carrero Bator <rcarrero@unbosque.edu.co>
2025-05-30 00:54:52 -0500

    Cambios rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent,FooterComponent],
+  imports: [RouterOutlet,HeaderComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
