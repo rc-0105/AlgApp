3d7f902a54d3bd3bef30f42df5436a03dbc71029
Ricardo Alberto Carrero Bator <rcarrero@unbosque.edu.co>
2025-05-30 00:48:27 -0500

    Auto-generated commit

--- server/chunk-ON2U6Z67.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{Fa as a,Ta as n,Ua as o,ab as s}from"./chunk-YKAFVU7E.mjs";import"./chunk-X2SEQXRR.mjs";var i=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=a({type:t,selectors:[["app-diagrams"]],decls:2,vars:0,template:function(e,r){e&1&&(n(0,"p"),s(1,"diagrams works!"),o())},encapsulation:2})};export{i as DiagramsComponent};
